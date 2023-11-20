3717e59460228d8b40c78757e9b81373dfdfc1d4
Irfanul Islam Jishan <47252675+jishan101@users.noreply.github.com>
2023-11-20 06:08:00 +0600

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <div className="z-0 relative min-h-[90vh] min-w-full flex content-center items-center">
       <div className=" z-10 mx-auto items-center -mt-[20%] lg:-mt-[10%]">
-        <div className="font-bebas sm:font-valencia text-[60px] sm:text-[100px] md:text-[125px] lg:text-[150px] text-primary-color text-center">
+        <div className="font-valencia text-[60px] sm:text-[100px] md:text-[125px] lg:text-[150px] text-primary-color text-center">
           Irfanul Islam
         </div>
         <div className="font-league-spartan font-bold text-action-color text-center text-[35px] sm:text-[54px] lg:text-[73px]">
